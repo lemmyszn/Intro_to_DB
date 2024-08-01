df427aabff37ffbe470d20966528985a9d4ffe97
lemmyszsn <lemmyxking@gmail.com>
2024-08-01 10:07:06 +0300

    add script that creates all the tables below in alx_book_store in your MySQL server.

--- task_2.sql
+++ task_2.sql
@@ -19,9 +19,9 @@ CREATE TABLE IF NOT EXISTS Books (
 
 CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(100) NOT NULL,
-    last_name VARCHAR(100) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
+    first_name VARCHAR(215) NOT NULL,
+    last_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) NOT NULL UNIQUE,
     phone VARCHAR(15)
 );
 
